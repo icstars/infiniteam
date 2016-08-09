50f964b7e9b34e27018a27439238cbb6106c7201
Eric <ehall@icstars.org>
2016-08-09 18:43:04 +0000

    [Eric]changed styling to css page added landing page added linking game page all in frontend folder

---
+++ frontend/js/landing.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#menuOptions').hide();
+        
+$('#menuOptionsButton').click(function() {
+    $('#menuOptions').show();
+    
+})
+                                 
+ $("#startButton").click(function() {
+    window.location = "gamepage.html" 
+ });  
+ 
+    $("#homeButton").click(function() {
+    window.location = "landing.html"                   
+                       });
+})
---
+++ frontend/js/shooter.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#menuOptions').hide();
+        
+$('#menuOptionsButton').click(function() {
+    $('#menuOptions').show();
+    
+})
+                                 
+ $("#startButton").click(function() {
+    window.location = "thegamelink.html" 
+ });  
+ 
+    $("#homeButton").click(function() {
+    window.location = "landing.html"                   
+                       });
+})
