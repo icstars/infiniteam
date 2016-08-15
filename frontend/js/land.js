dbc16c48561147f62b755568c14e87eedd8348af
Anna Brukh <gbrukh@icstars.org>
2016-08-15 14:21:11 +0000

    1st commit-loginpage

---
+++ frontend/js/land.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   $('#menuOptions').hide();
+   
+   
+   $('#menuOptionsButton').click(function() {
+       $('#menuOptions').show();
+   })
+   
+   
+   
+   $('#startbutton').click(function() {
+       window.open("./startpage.html");
+   })
+   }
+)
