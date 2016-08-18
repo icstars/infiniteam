9b41268d68a5271a164ea14db39ed7f58285321f
Anna Brukh <gbrukh@icstars.org>
2016-08-18 14:31:23 +0000

    Created bank page and styled it.Improved style of loginpage

---
+++ javascript/landing.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#menuOptions').hide();
+        
+$('#menuOptionsButton').click(function() {
+    $('#menuOptions').show();
+    
+})
+                                 
+// $("#startButton").click(function() {
+ //   window.location = "gamepage.html" 
+// })  
+  $("#startButton").click(function() {
+    window.location = "samplegame.html" 
+ }) 
+    $("#homeButton").click(function() {
+    window.location = "landing.html"                   
+ })
+    $("#bankPage").click(function() {
+        window.location = "Bank.html"
+    });
+})
+
+    
